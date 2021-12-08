f67e33b83c35e85e377d9e12818a48253559a453
AloisLeg <77205332+AloisLeg@users.noreply.github.com>
2021-12-08 14:14:02 +0100

    Add files via upload

---
+++ Auteur.cpp
@@ -0,0 +1,21 @@
+#include "auteur.h"
+
+Auteur::Auteur(int id, std::string nom, std::string prenom, Date datenaissance) : _id(id), _nom(nom), _prenom(prenom), _datenaissance(datenaissance) {
+
+} 
+
+int Auteur::id() const {
+	return _id;
+}
+
+std::string Auteur::nom() const {
+	return _nom;
+}
+
+std::string Auteur::prenom() const {
+	return _prenom;
+}
+
+Date Auteur::datenaissance() const {
+	return _datenaissance;
+}
