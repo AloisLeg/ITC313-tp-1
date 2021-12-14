f8485748a35d4d0796c17d93bbe0a9819ffd98e3
Mélou <melloup12@gmail.com>
2021-12-14 08:14:48 +0100

    C'est le test lol

--- Auteur/Auteur.cpp
+++ Auteur/Auteur.cpp
@@ -1,7 +1,7 @@
 #include "auteur.h"
 
 Auteur::Auteur(int id, std::string nom, std::string prenom, Date datenaissance) : _id(id), _nom(nom), _prenom(prenom), _datenaissance(datenaissance) {
-
+//Ceci est un test
 } 
 
 int Auteur::id() const {
